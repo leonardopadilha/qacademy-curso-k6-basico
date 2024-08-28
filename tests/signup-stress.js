7ec3a6a15b8f81e191bdf63c5bef0260583d581c
leonardopadilha <leonardopadilha28@gmail.com>
2024-08-28 08:05:38 -0300

    Curso básico k6 - QAx

--- tests/signup-stress.js
+++ tests/signup-stress.js
@@ -1,18 +1,17 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6'
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
-
-export function handleSummary(data) {
-  return {
-    "summary.html": htmlReport(data),
-  };
-}
 
 export const options = {
   stages: [
-    { duration: '1m', target: 100 },
     { duration: '2m', target: 100 },
-    { duration: '1m', target: 0 },
+    { duration: '5m', target: 100 },
+    { duration: '2m', target: 200 },
+    { duration: '5m', target: 200 },
+    { duration: '2m', target: 300 },
+    { duration: '5m', target: 300 },
+    { duration: '2m', target: 400 },
+    { duration: '5m', target: 400 },
+    { duration: '10m', target: 0 },
   ],
   thresholds: {
     http_req_duration: ['p(95)<2000'], // 95% das requisicoes deve responder em até 2s
